803463e57575ea7dfd971501660f423ce6c449da
kaingadeaniruddha21 <93026301+kaingadeaniruddha21@users.noreply.github.com>
2022-03-11 13:59:26 +0530

    Delete OnlineFlightTicketBookingApplication-1 directory

--- OnlineFlightTicketBookingApplication-1/src/main/java/com/mouritech/onlineflightticketbookingapplication/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.mouritech.onlineflightticketbookingapplication.exception;
-
-public class UserNotFoundException extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public UserNotFoundException(String message) {
-		super(message);
-	}
-}
